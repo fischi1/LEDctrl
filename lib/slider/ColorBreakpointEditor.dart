5286842fecddba2c9c18dc73bf961bf3cfb9748c
Lukas <fischi.ftw@gmail.com>
2020-06-01 16:44:05 +0200

    moving the editor with the pos

--- lib/slider/ColorBreakpointEditor.dart
+++ lib/slider/ColorBreakpointEditor.dart
@@ -36,66 +36,73 @@ class ColorBreakpointEditor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
+      padding: const EdgeInsets.fromLTRB(0, 120, 0, 40),
       child: Container(
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(139, 0, 0, 0),
-          borderRadius: BorderRadius.all(
-            Radius.circular(10),
-          ),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        height: double.infinity,
+        child: UnconstrainedBox(
+          constrainedAxis: Axis.horizontal,
+          alignment: Alignment(0, colorBreakpoint.position * 2 - 1),
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(139, 0, 0, 0),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.check),
-                  onPressed: onSubmit,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.check),
+                      onPressed: onSubmit,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: onDelete,
+                    )
+                  ],
                 ),
-                IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: onDelete,
+                Slider(
+                  activeColor: Colors.red,
+                  max: 255,
+                  value: colorBreakpoint.color.red + 0.0,
+                  onChanged: (val) {
+                    handleColorValueChange(val.floor(), ColorChannel.red);
+                  },
+                ),
+                Slider(
+                  activeColor: Colors.green,
+                  max: 255,
+                  value: colorBreakpoint.color.green + 0.0,
+                  onChanged: (val) {
+                    handleColorValueChange(val.floor(), ColorChannel.green);
+                  },
+                ),
+                Slider(
+                  activeColor: Colors.blue,
+                  max: 255,
+                  value: colorBreakpoint.color.blue + 0.0,
+                  onChanged: (val) {
+                    handleColorValueChange(val.floor(), ColorChannel.blue);
+                  },
+                ),
+                Slider(
+                  activeColor: Colors.white,
+                  value: colorBreakpoint.brightnessMultiplier,
+                  onChanged: handleBrightnessMultiplerChange,
+                ),
+                Text(
+                  "${colorBreakpoint.color.red}, ${colorBreakpoint.color.green}, ${colorBreakpoint.color.blue}, ${(colorBreakpoint.brightnessMultiplier * 100).roundToDouble() / 100}",
+                ),
+                SizedBox(
+                  height: 12,
                 )
               ],
             ),
-            Slider(
-              activeColor: Colors.red,
-              max: 255,
-              value: colorBreakpoint.color.red + 0.0,
-              onChanged: (val) {
-                handleColorValueChange(val.floor(), ColorChannel.red);
-              },
-            ),
-            Slider(
-              activeColor: Colors.green,
-              max: 255,
-              value: colorBreakpoint.color.green + 0.0,
-              onChanged: (val) {
-                handleColorValueChange(val.floor(), ColorChannel.green);
-              },
-            ),
-            Slider(
-              activeColor: Colors.blue,
-              max: 255,
-              value: colorBreakpoint.color.blue + 0.0,
-              onChanged: (val) {
-                handleColorValueChange(val.floor(), ColorChannel.blue);
-              },
-            ),
-            Slider(
-              activeColor: Colors.white,
-              value: colorBreakpoint.brightnessMultiplier,
-              onChanged: handleBrightnessMultiplerChange,
-            ),
-            Text(
-              "${colorBreakpoint.color.red}, ${colorBreakpoint.color.green}, ${colorBreakpoint.color.blue}, ${(colorBreakpoint.brightnessMultiplier * 100).roundToDouble() / 100}",
-            ),
-            SizedBox(
-              height: 12,
-            )
-          ],
+          ),
         ),
       ),
     );
