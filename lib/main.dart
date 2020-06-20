ec09322482091eb4ee93212980bae7d1fa535b12
Lukas <fischi.ftw@gmail.com>
2020-06-20 10:30:54 +0200

    separated main page from main file, moved components into a separate folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
-import 'package:fischi/GradientBreakpointBackground.dart';
-import 'package:fischi/TransparentGradientAppBar.dart';
-import 'package:fischi/api/SetPreset.dart';
-import 'package:fischi/api/Toggle.dart';
-import 'package:fischi/domain/ColorBreakpoint.dart';
-import 'package:fischi/slider/ColorBreakpointEditor.dart';
-import 'package:fischi/slider/ColorSlider.dart';
-import 'package:fischi/slider/SliderAnimatedAlign.dart';
+import 'package:fischi/views/SimplePresetPage.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
-//https://github.com/mchome/flutter_colorpicker/blob/master/lib/src/hsv_picker.dart
 
 class MyApp extends StatelessWidget {
   @override
@@ -21,128 +12,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         secondaryHeaderColor: Colors.white,
       ),
-      home: MyHomePage(),
+      home: SimplePresetPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<ColorBreakpoint> breakpoints;
-  String selectedBreakpointId;
-  bool onOffToggle = true;
-
-  SetPreset setPreset = SetPreset();
-
-  @override
-  void initState() {
-    super.initState();
-    breakpoints = List();
-    breakpoints.add(ColorBreakpoint(
-      color: Colors.orange,
-      position: 0.1,
-    ));
-    breakpoints.add(ColorBreakpoint(
-      color: Colors.purple,
-      position: 0.5,
-    ));
-  }
-
-  void handleBreakpointChange(List<ColorBreakpoint> newBreakpoints) {
-    setPreset.setSimple(newBreakpoints);
-    setState(() {
-      breakpoints = newBreakpoints;
-    });
-  }
-
-  Widget _buildBreakpointEditor() {
-    if (selectedBreakpointId == null) return Container();
-
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(
-        110,
-        0,
-        15,
-        0,
-      ),
-      child: ColorBreakpointEditor(
-        colorBreakpoint: breakpoints[
-            breakpoints.indexWhere((cb) => cb.id == selectedBreakpointId)],
-        onChange: (changedBreakpoint) {
-          var newList = List.of(breakpoints);
-          newList[newList.indexWhere((cb) => cb.id == changedBreakpoint.id)] =
-              changedBreakpoint;
-          handleBreakpointChange(newList);
-        },
-        onSubmit: () {
-          setState(() {
-            selectedBreakpointId = null;
-          });
-        },
-        onDelete: () {
-          var newList = List.of(breakpoints);
-          newList.removeWhere((cb) => cb.id == selectedBreakpointId);
-          setState(() {
-            handleBreakpointChange(newList);
-            selectedBreakpointId = null;
-          });
-        },
-      ),
-    );
-  }
-
-  HSVColor currentColor = HSVColor.fromColor(Colors.green);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: TransparentGradientAppBar(
-          toggleValue: onOffToggle,
-          onToggleChange: (val) {
-            Toggle.toggleOnOff(val);
-            setState(() {
-              onOffToggle = val;
-            });
-          },
-        ),
-        extendBodyBehindAppBar: true,
-        body: Container(
-          decoration:
-              GradientBreakpointBackground.buildGradientBackground(breakpoints),
-          child: Stack(
-            children: <Widget>[
-              SliderAnimatedAlign(
-                right: selectedBreakpointId == null,
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(
-                    0,
-                    120,
-                    0,
-                    40,
-                  ),
-                  child: ColorSlider(
-                    breakpoints: breakpoints,
-                    onChange: (newBreakpoints) {
-                      setPreset.setSimple(newBreakpoints);
-                      setState(() {
-                        breakpoints = newBreakpoints;
-                      });
-                    },
-                    onSelectBreakPoint: (breakpoint) {
-                      setState(() {
-                        selectedBreakpointId = breakpoint.id;
-                      });
-                    },
-                  ),
-                ),
-              ),
-              _buildBreakpointEditor()
-            ],
-          ),
-        ));
   }
 }
