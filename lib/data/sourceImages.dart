2374b8b2dda5a5d883674f6a2208b13b3e97b477
Lukas <fischi.ftw@gmail.com>
2020-07-19 22:45:29 +0200

    more image presets

--- lib/data/sourceImages.dart
+++ lib/data/sourceImages.dart
@@ -199,4 +199,358 @@ List<SourceImage> sourceImages = [
       ),
     ],
   ),
+  SourceImage(
+    imagePath: "assets/images/bloom-blooming-blossom-blur.jpg",
+    author: "Pixabay",
+    url: "https://www.pexels.com/photo/bloom-blooming-blossom-blur-355663/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(93, 255, 0, 1)),
+        position: 0,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(164, 35, 240, 1)),
+        position: 0.15,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(146, 2, 244, 1)),
+        position: 0.61,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/low-angle-photo-of-red-and-orange-umbrella.jpg",
+    author: "Peter Fazekas",
+    url:
+        "https://www.pexels.com/photo/low-angle-photo-of-red-and-orange-umbrella-1170594/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 10, 0, 1)),
+        position: 0.25,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 76, 0, 1)),
+        position: 0.26,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 76, 0, 1)),
+        position: 0.45,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 10, 0, 1)),
+        position: 0.46,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 10, 0, 1)),
+        position: 0.7,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(70, 160, 180, 1)),
+        position: 0.71,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/pink-and-white-flowers-in-tilt-shift-lens.jpg",
+    author: "Brett Sayles",
+    url:
+        "https://www.pexels.com/photo/pink-and-white-flowers-in-tilt-shift-lens-4212936/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(98, 59, 159, 1)),
+        position: 0,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(248, 113, 113, 1)),
+        position: 0.19,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(98, 59, 159, 1)),
+        position: 0.48,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(248, 113, 113, 1)),
+        position: 0.71,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(98, 59, 159, 1)),
+        position: 0.83,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(248, 113, 113, 1)),
+        position: 1,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/silhouette-of-mountains.jpg",
+    author: "Simon Matzinger",
+    url: "https://www.pexels.com/photo/silhouette-of-mountains-1323550/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(80, 201, 255, 1)),
+        position: 0,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(224, 174, 48, 1)),
+        position: 0.33,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(242, 103, 48, 1)),
+        position: 0.66,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(210, 79, 144, 1)),
+        position: 1,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/close-up-photo-of-pizza-near-bonfire.jpg",
+    author: "Vinicius Benedit",
+    url:
+        "https://www.pexels.com/photo/close-up-photo-of-pizza-near-bonfire-1082343/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 65, 0, 1)),
+        position: 0,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 0, 0, 1)),
+        position: 0.14,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 65, 0, 1)),
+        position: 0.29,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 0, 0, 1)),
+        position: 0.43,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 65, 0, 1)),
+        position: 0.57,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 0, 0, 1)),
+        position: 0.71,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 65, 0, 1)),
+        position: 0.86,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 0, 0, 1)),
+        position: 1,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/waterfall-near-trees.jpg",
+    author: "Roni Alfasi",
+    url: "https://www.pexels.com/photo/waterfall-near-trees-2737248/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(16, 15, 15, 1)),
+        position: 0,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(172, 163, 89, 1)),
+        position: 0.18,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(170, 194, 64, 1)),
+        position: 0.41,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(117, 140, 42, 1)),
+        position: 0.69,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(16, 15, 15, 1)),
+        position: 1,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/seaport-during-daytime.jpg",
+    author: "Pok Rie",
+    url: "https://www.pexels.com/photo/seaport-during-daytime-132037/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(75, 146, 189, 1)),
+        position: 0.07,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 86, 0, 1)),
+        position: 0.14,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 86, 0, 1)),
+        position: 0.87,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(75, 146, 189, 1)),
+        position: 0.93,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/close-up-of-fruits-in-bowl.jpg",
+    author: "Pixabay",
+    url: "https://www.pexels.com/photo/close-up-of-fruits-in-bowl-327098/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(226, 84, 0, 1)),
+        position: 0.12,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(71, 143, 0, 1)),
+        position: 0.2,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(226, 84, 0, 1)),
+        position: 0.28,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(226, 84, 0, 1)),
+        position: 0.64,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(71, 143, 0, 1)),
+        position: 0.72,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(226, 84, 0, 1)),
+        position: 0.8,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/seashore-scenery.jpg",
+    author: "Greg",
+    url: "https://www.pexels.com/photo/seashore-scenery-2418664/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(140, 44, 255, 1)),
+        position: 0.12,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 211, 242, 1)),
+        position: 0.17,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(140, 44, 255, 1)),
+        position: 0.22,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(140, 44, 255, 1)),
+        position: 0.27,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 211, 242, 1)),
+        position: 0.32,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(140, 44, 255, 1)),
+        position: 0.37,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(140, 44, 255, 1)),
+        position: 0.42,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 211, 242, 1)),
+        position: 0.47,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(140, 44, 255, 1)),
+        position: 0.52,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/crystal-glass-on-a-colorful-background.jpg",
+    author: "Steve Johnson",
+    url:
+        "https://www.pexels.com/photo/crystal-glass-on-a-colorful-background-2179374/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 0, 44, 1)),
+        position: 0.16,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(35, 255, 0, 1)),
+        position: 0.56,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(1, 45, 92, 1)),
+        position: 0.9,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath: "assets/images/multicolored-abstract-painting.jpg",
+    author: "Steve Johnson",
+    url: "https://www.pexels.com/photo/multicolored-abstract-painting-1548111/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 0, 186, 1)),
+        position: 0.06,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(254, 67, 1, 1)),
+        position: 0.14,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(219, 160, 2, 1)),
+        position: 0.27,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(219, 160, 2, 1)),
+        position: 0.47,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(248, 3, 255, 1)),
+        position: 0.51,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(248, 3, 255, 1)),
+        position: 0.69,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(0, 177, 255, 1)),
+        position: 0.8,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(0, 177, 255, 1)),
+        position: 0.82,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 34, 0, 1)),
+        position: 0.87,
+      ),
+    ],
+  ),
+  SourceImage(
+    imagePath:
+        "assets/images/green-trees-under-blue-and-orange-sky-during-sunset.jpg",
+    author: "Lisa Fotios",
+    url:
+        "https://www.pexels.com/photo/green-trees-under-blue-and-orange-sky-during-sunset-1107717/",
+    breakpoints: [
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 114, 0, 1)),
+        position: 0.07,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(167, 201, 224, 1)),
+        position: 0.17,
+      ),
+      ColorBreakpoint(
+        color: HSVColor.fromColor(Color.fromRGBO(255, 114, 0, 1)),
+        position: 0.27,
+      ),
+    ],
+  ),
 ];
